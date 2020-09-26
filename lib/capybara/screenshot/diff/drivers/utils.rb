c4fbc35a30cb7f1561a33a19357be209423b4f66
Uwe Kubosch <uwe@datek.no>
2020-09-26 22:30:26 +0200

    Allow probing for vips without raising

--- lib/capybara/screenshot/diff/drivers/utils.rb
+++ lib/capybara/screenshot/diff/drivers/utils.rb
@@ -6,8 +6,16 @@ module Capybara
       module Utils
         def self.detect_available_drivers
           result = []
-          result << :vips if defined?(Vips) || require('vips')
-          result << :chunky_png if defined?(ChunkyPNG) || require('chunky_png')
+          begin
+            result << :vips if defined?(Vips) || require('vips')
+          rescue LoadError
+            # vips not present
+          end
+          begin
+            result << :chunky_png if defined?(ChunkyPNG) || require('chunky_png')
+          rescue LoadError
+            # chunky_png not present
+          end
           result
         end
       end
