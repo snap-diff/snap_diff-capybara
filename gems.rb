5126ef2bc31d6afd15cbde9644bf4449e425f18e
Paul Keen <125715+pftg@users.noreply.github.com>
2020-12-05 18:14:38 +0200

    Moves tests and development gems from gemspec

--- gems.rb
+++ gems.rb
@@ -5,7 +5,24 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in capybara-screenshot-diff.gemspec
 gemspec path: __dir__
 
-gem "oily_png", platform: :ruby
+gem "rake"
 
-gem "image_processing", require: false
+# Image processing libraries
+gem "oily_png", platform: :ruby
 gem "ruby-vips", require: false
+
+# Test
+gem "minitest", require: false
+gem "minitest-stub-const", require: false
+gem "simplecov", require: false
+
+# Capybara
+gem "puma", require: false
+
+# Capybara Drivers
+gem "selenium-webdriver", require: false
+gem "webdrivers", require: false
+
+group :tools do
+  gem "standard", require: false
+end
