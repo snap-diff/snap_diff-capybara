50e9d1067d05c07711fe5cbe4ad9d4dd8587e20c
Paul Keen <125715+pftg@users.noreply.github.com>
2020-12-16 17:50:09 +0200

    Fixes flaky tests on CI
    * Increases timeouts for chrome to start or process requests on CI
    * Removes problem when we have serveral drivers usage for counting leaking processes
    * For matrixes tests we used default bundler instead of gemfiles. Added
      gemfile selection for all stpes on CI
    * There were double bundle install, removed redundent bundle install for
      default gemfile
    * Caret problem: added more time to make sure we have a stable screenshot.
    * Make lint job to fail if there are lint errors in the code

--- test/integration/test_methods_system_test.rb
+++ test/integration/test_methods_system_test.rb
@@ -1,30 +1,36 @@
 # frozen_string_literal: true
 
-require "action_pack/version"
-
-ENV["CAPYBARA_DRIVER"] ||= "selenium_chrome_headless"
-
 require "system_test_case"
 
-unless [ActionPack::VERSION::MAJOR, ActionPack::VERSION::MINOR].join >= "60" && ENV["CAPYBARA_DRIVER"].include?("chrome")
-  warn "Regression only for 6x Rails and Chrome"
+# NOTE: For this test we need only chrome browser,
+#       because we can spot problem by counting running chrome driver processes
+require "action_pack/version"
+unless [ActionPack::VERSION::MAJOR, ActionPack::VERSION::MINOR].join >= "60" && ENV["CAPYBARA_DRIVER"].include?("selenium_chrome")
+  warn "Regression only for 6x Rails and driven_by construction"
   return
 end
+
+require "objspace"
 
 module Capybara
   module Screenshot
     module Diff
       class TestMethodsSystemTest < ActionDispatch::SystemTestCase
-        driven_by :selenium, using: :headless_chrome
-
-        teardown { Capybara.use_default_driver }
-
         include TestMethods
         include TestHelper
 
-        def test_current_capybara_driver_class_do_not_spawn_new_process
-          2.times { current_capybara_driver_class }
-          assert_equal 1, `ps x -o pid= -o command=`.scan("webdrivers/chromedriver").size
+        driven_by :selenium, using: :headless_chrome
+
+        def test_current_capybara_driver_class_do_not_spawn_new_process_when_we_use_system_test_cases
+          # NOTE: There is possible that we have several drivers usage in the one suite,
+          #       so each of them will have separate instance
+          other_activated_drivers = ObjectSpace.each_object(Capybara::Selenium::Driver).count
+
+          3.times { current_capybara_driver_class }
+
+          run_chrome_drivers = ObjectSpace.each_object(Capybara::Selenium::Driver).count
+          assert run_chrome_drivers.positive?
+          assert run_chrome_drivers - other_activated_drivers <= 1
         end
       end
     end
