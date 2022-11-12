6d12142cc5e6cd42b1ac9300f0489b3cc007aa2e
Paul Keen <125715+pftg@users.noreply.github.com>
2022-11-12 13:09:27 +0100

    Adds tests for skip_area usage

--- test/integration/record_screenshot_test.rb
+++ test/integration/record_screenshot_test.rb
@@ -5,11 +5,13 @@ require "system_test_case"
 class RecordScreenshotTest < SystemTestCase
   def test_record_index
     visit "/"
+
     screenshot "index"
   end
 
   def test_record_index_cropped
     visit "/"
-    screenshot "index-cropped", crop: rect_for("form")
+
+    screenshot "index-cropped", crop: "form"
   end
 end
