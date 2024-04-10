b20af40e821ea7818565f3780a209747b6d3afd5
Paul Keen <125715+pftg@users.noreply.github.com>
2024-04-10 17:25:05 +0200

    test: fixes signature tests

--- test/capybara/screenshot/diff/screenshoter_test.rb
+++ test/capybara/screenshot/diff/screenshoter_test.rb
@@ -2,6 +2,7 @@
 
 require "test_helper"
 require "minitest/mock"
+require "capybara/screenshot/diff/drivers/chunky_png_driver"
 
 module Capybara
   module Screenshot
@@ -10,7 +11,7 @@ module Capybara
       include Diff::TestMethodsStub
 
       test "#take_screenshot without wait skips image loading" do
-        screenshoter = Screenshoter.new({wait: nil}, Diff::Drivers.for)
+        screenshoter = Screenshoter.new({wait: nil}, Diff::Drivers::ChunkyPNGDriver.new)
 
         mock = ::Minitest::Mock.new
         mock.expect(:save_screenshot, true) { |path| path.include?("01_a.png") }
@@ -24,8 +25,10 @@ module Capybara
         mock.verify
       end
 
-      test "#prepare_page_for_screenshot without wait skips image loading" do
-        Screenshoter.new({wait: nil}, Diff::Drivers.for).prepare_page_for_screenshot(timeout: nil)
+      test "#prepare_page_for_screenshot without wait does not raise any error" do
+        screenshoter = Screenshoter.new({wait: nil}, Diff::Drivers::ChunkyPNGDriver.new)
+
+        screenshoter.prepare_page_for_screenshot(timeout: nil) # does not raise an error
       end
     end
   end
