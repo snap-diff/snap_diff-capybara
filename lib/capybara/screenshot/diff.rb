a1cb13e3f9bad2ab752e0dc260c8c13d46cd2a20
Paul Keen <125715+pftg@users.noreply.github.com>
2024-07-13 22:03:19 +0200

    chore: adds deprecation warnings

--- lib/capybara/screenshot/diff.rb
+++ lib/capybara/screenshot/diff.rb
@@ -2,9 +2,9 @@
 
 warn <<~MSG
 
-    DEPRECATED: use 'require "capybara_screenshot_diff/minitest"' instead of 'require "capybara/screenshot/diff"'
-        in #{caller(3)&.first}. 
-        "capybara/screenshot/diff" is no longer needed and will be removed in the next major release.
+  DEPRECATED: use 'require "capybara_screenshot_diff/minitest"' instead of 'require "capybara/screenshot/diff"'
+      in #{caller(3)&.first}. 
+      "capybara/screenshot/diff" is no longer needed and will be removed in the next major release.
 
 MSG
 
